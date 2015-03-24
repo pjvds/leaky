c05375cee97b961b3a5cd1f0098c2d77984b105a
Pieter Joost van de Sande <pj@born2code.net>
2015-03-24 20:03:16 +0100

    add data types

--- main.go
+++ main.go
@@ -8,15 +8,38 @@ import (
 	"time"
 )
 
+type Snapshot struct {
+	// bytes allocated and still in use
+	Alloc uint64
+
+	// total number of allocated objects
+	HeapObjects uint64
+
+	// total number of GC cycles
+	NumGC uint32
+}
+
+type Diff struct {
+	Before Snapshot
+	After  Snapshot
+
+	Change
+}
+
+type Change struct {
+	Alloc       uint64
+	HeapObjects uint64
+}
+
 func main() {
 	var stats runtime.MemStats
-	timer := time.NewTimer(1 * time.Second)
+	tick := time.Tick(1 * time.Second)
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
 
 	for {
 		select {
-		case <-timer.C:
+		case <-tick:
 			runtime.ReadMemStats(&stats)
 			fmt.Printf("%+v\n\n\n", stats)
 		case <-interrupt:
